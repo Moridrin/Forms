510ea13653e56956b6e7b40887caadce6aa73578
Jeroen Berkvens <Account@Moridrin.com>
2017-12-27 17:32:03 +0100

    Forms Plugin Setup

---
+++ ssv-forms.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: SSV Forms
+ * Plugin URI: http://moridrin.com/ssv-forms
+ * Description: This is a plugin to create forms with ease.
+ * Version: 0.1.0
+ * Author: Jeroen Berkvens
+ * Author URI: http://nl.linkedin.com/in/jberkvens/
+ * License: WTFPL
+ * License URI: http://www.wtfpl.net/txt/copying/
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+/** @var wpdb $wpdb */
+define('SSV_FORMS_PATH', plugin_dir_path(__FILE__));
+define('SSV_FORMS_URL', plugins_url() . '/ssv-forms/');
+require_once 'general/base/SSV_Base.php';
+
+if (!in_array(realpath(SSV_FORMS_PATH . 'general/forms/SSV_Forms.php'), get_included_files())) {
+    require_once 'general/forms/SSV_Forms.php';
+}
