9ca0373703fab0ee6358e7b76e70fece37fdf189
Jeroen Berkvens <Account@Moridrin.com>
2018-04-24 16:54:26 +0200

    Model working 0.8

--- ssv-forms.php
+++ ssv-forms.php
@@ -14,9 +14,4 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require_once 'general/base/base.php';
-require_once 'general/forms/forms.php';
+require_once 'general/general.php';
